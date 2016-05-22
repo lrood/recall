9bd9df0f23d6719f628139346d1b8a0d8b67a909
lrood <laurenrood1@gmail.com>
2016-05-22 14:25:44 -0500

    changes to recall program

--- recall.py
+++ recall.py
@@ -10,4 +10,15 @@ model = model.upper()
 
 file = open('FLAT_RCL.txt', 'r', errors = 'ignore')
 
+if make == "":
+	print("Please enter a valid make, model, and year.")
+if model == "":
+	print("Please enter a valid make, model, and year.")
+if year == "":
+	print("Please enter a valid make, model, and year.")
 
+for line in file:
+	x = line.split("\t")
+	if x[2] == make and x[3] == model and x[4] == year:
+		print (x[6], x[21])
+		print ('\n')
